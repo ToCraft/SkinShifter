663b44e4ba8f55c74f80b8133c4a9046d82f1299
To_Craft <development@tocraft.dev>
2026-05-15 21:05:14 +0200

    update ModMaster

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
 plugins {
-    id("dev.tocraft.modmaster.root") version ("2.3")
+    id("dev.tocraft.modmaster.root") version ("2.4-SNAPSHOT")
 }
 
 subprojects {
     repositories {
-        mavenLocal()
         maven("https://maven.fabricmc.net/") // fabric api
         maven("https://maven.terraformersmc.com/releases/") // mod menu mod
         maven("https://maven.shedaniel.me/") // cloth config
